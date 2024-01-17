48b29eee2a5fad1605c11563f9d30d8d5a1c0e5f
Venera Avagyan <venera.avagyan@gmail.com>
2024-01-17 15:40:27 -0500

    modified:   cypress/e2e/travel_site_flights_tests.sy.js 	new file:   cypress/fixtures/params.json

--- cypress/e2e/travel_site_flights_tests.sy.js
+++ cypress/e2e/travel_site_flights_tests.sy.js
@@ -1,4 +1,5 @@
 ///<reference types="cypress"/>
+import { data } from "../fixtures/params.json"
 
 describe('Flights functionality verification', () => {
 
@@ -18,28 +19,31 @@ describe('Flights functionality verification', () => {
     })
 
     it('navigate to the Flights tab', () => {
+        // cy.viewport(400, 300)
+        // cy.screenshot('flight_page_screen')
+
         cy.get(origin).should("be.visible")
         cy.get(flightTab).invoke('attr', 'aria-expanded').should('eq', 'true')
         cy.get(flightTab).invoke('text').should('eq', 'flight')
     })
 
     it('populate Origin and Destination fields', () => {
-        cy.get(origin).type('Paris').invoke("val").should("eq", "Paris")
-        // cy.get(origin).type('Paris').should('have.value', 'Paris')
-        cy.get(destination).type('London').invoke("val").should("eq", "London")
-        // cy.get(destination).type('London').should('have.value', "London")
+        data.forEach(element => {
+            cy.get(origin).clear().type(element.from).invoke("val").should("eq", element.from)
+            cy.get(destination).clear().type(element.to).invoke("val").should("eq", element.to)
+        })
     })
 
-    it('populate CheckIn and CheckOut dates', () => {
-        cy.get(checkIn).type('10/10/2025').invoke('val').should('eq', '10/10/2025')
-        cy.get(checkOut).clear().type('10/10/2026').invoke('val').should('eq', '10/10/2026')
-    })
+    // it('populate CheckIn and CheckOut dates', () => {
+    //     cy.get(checkIn).type('10/10/2025').invoke('val').should('eq', '10/10/2025')
+    //     cy.get(checkOut).clear().type('10/10/2026').invoke('val').should('eq', '10/10/2026')
+    // })
 
-    it('check radio buttons', () => {
-        cy.get(businessRadioBtn).click().should('be.checked')
-        cy.get(economyRadioBtn).click().should('be.checked')
-        cy.get(firstClassRadioBtn).click().should('be.checked')
-    })
+    // it('check radio buttons', () => {
+    //     cy.get(businessRadioBtn).click().should('be.checked')
+    //     cy.get(economyRadioBtn).click().should('be.checked')
+    //     cy.get(firstClassRadioBtn).click().should('be.checked')
+    // })
 
 
 })
